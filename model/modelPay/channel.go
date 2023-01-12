98460bf4760aeb7b9f6e104642a2ecd57c3650e7
wuhuboom <wuhuboom99@gmail.com>
2023-01-12 18:15:24 +0800

    2023年1月12日18:15:24

--- model/modelPay/channel.go
+++ model/modelPay/channel.go
@@ -48,7 +48,7 @@ func (ch *Channel) GetUpi(db *gorm.DB) (Bank, error) {
 	}
 	for _, bank := range CB {
 		//判断今日已经占用的金额
-		db.Raw("SELECT sum(amount)  as  sum_pull  FROM collections  WHERE bank_id =?  and  date =  ? and  release_time  > ?  and  status = 1", bank.ID, time.Now().Format("2006-01-02"), time.Now().Unix()).Scan(&TallData)
+		db.Debug().Raw("SELECT sum(amount)  as  sum_pull  FROM collections  WHERE bank_id =?  and  date =  ? and  release_time  > ?  and  status = 1", bank.ID, time.Now().Format("2006-01-02"), time.Now().Unix()).Scan(&TallData)
 		//比大小
 		Ba := Bank{}
 		err := db.Where("id=?", bank.BankId).First(&Ba).Error
