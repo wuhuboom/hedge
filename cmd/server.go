1bd43d7b1ef15752fe7f5f240f1b6c9de9b940a1
wuhuboom <wuhuboom99@gmail.com>
2023-02-14 22:19:20 +0800

    2023年2月14日22:19:19

--- cmd/server.go
+++ cmd/server.go
@@ -14,6 +14,7 @@ import (
 	"github.com/wangyi/GinTemplate/dao/mysql"
 	"github.com/wangyi/GinTemplate/dao/redis"
 	"github.com/wangyi/GinTemplate/logger"
+	"github.com/wangyi/GinTemplate/model"
 	"github.com/wangyi/GinTemplate/process"
 	"github.com/wangyi/GinTemplate/router"
 	"github.com/wangyi/GinTemplate/setting"
@@ -92,7 +93,7 @@ func run(cmd *cobra.Command, args []string) {
 
 	go func() {
 		for true {
-
+			mysql.DB.Find(&model.Runner{})
 		}
 	}()
 
