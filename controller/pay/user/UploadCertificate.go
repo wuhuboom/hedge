6d6b31b252f7d9c5f96af9fbc481642b6f4a452e
whuboom <wuhuboom99@gamil.com>
2024-07-14 22:03:54 +0800

    收款

--- controller/pay/user/UploadCertificate.go
+++ controller/pay/user/UploadCertificate.go
@@ -142,7 +142,7 @@ func GetCertificateForImageRtt(c *gin.Context) {
 	//}
 	//fmt.Println(mer)
 	DA := make(map[string]string)
-	DA["host"] = viper.GetString("config.webUrl")
+	DA["host"] = viper.GetString("config.getCertificateForImageRtt")
 	DA["imageUrl"] = col.ProofOfPaymentImageUrl
 	if col.ProofOfPaymentImageUrl == "" {
 		tools.ReturnErr101Code(c, "")
